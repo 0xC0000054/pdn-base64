9026bfaf4de69ac8c102a43807b8524d454e8983
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2020-12-15 21:55:30 -0700

    Change the AutoScaleMode to Dpi

--- Base64SaveConfigWidget.Designer.cs
+++ Base64SaveConfigWidget.Designer.cs
@@ -51,9 +51,9 @@
             this.cssPanel.SuspendLayout();
             this.htmlPanel.SuspendLayout();
             this.SuspendLayout();
-            //
+            // 
             // formatCbo
-            //
+            // 
             this.formatCbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.formatCbo.FormattingEnabled = true;
             this.formatCbo.Items.AddRange(new object[] {
@@ -64,9 +64,9 @@
             this.formatCbo.Size = new System.Drawing.Size(134, 21);
             this.formatCbo.TabIndex = 1;
             this.formatCbo.SelectedIndexChanged += new System.EventHandler(this.formatCbo_SelectedIndexChanged);
-            //
+            // 
             // lineBreakCb
-            //
+            // 
             this.lineBreakCb.AutoSize = true;
             this.lineBreakCb.Location = new System.Drawing.Point(4, 52);
             this.lineBreakCb.Name = "lineBreakCb";
@@ -75,9 +75,9 @@
             this.lineBreakCb.Text = "Insert line break";
             this.lineBreakCb.UseVisualStyleBackColor = true;
             this.lineBreakCb.CheckedChanged += new System.EventHandler(this.lineBreakCb_CheckedChanged);
-            //
+            // 
             // uriEncodeCb
-            //
+            // 
             this.uriEncodeCb.AutoSize = true;
             this.uriEncodeCb.Location = new System.Drawing.Point(4, 76);
             this.uriEncodeCb.Name = "uriEncodeCb";
@@ -86,9 +86,9 @@
             this.uriEncodeCb.Text = "Data URI encoding";
             this.uriEncodeCb.UseVisualStyleBackColor = true;
             this.uriEncodeCb.CheckedChanged += new System.EventHandler(this.uriEncodeCb_CheckedChanged);
-            //
+            // 
             // uriDataTypeCbo
-            //
+            // 
             this.uriDataTypeCbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.uriDataTypeCbo.FormattingEnabled = true;
             this.uriDataTypeCbo.Items.AddRange(new object[] {
@@ -100,9 +100,9 @@
             this.uriDataTypeCbo.Size = new System.Drawing.Size(134, 21);
             this.uriDataTypeCbo.TabIndex = 5;
             this.uriDataTypeCbo.SelectedIndexChanged += new System.EventHandler(this.uriDataTypeCbo_SelectedIndexChanged);
-            //
+            // 
             // cssPanel
-            //
+            // 
             this.cssPanel.Controls.Add(this.cssClassNameTxt);
             this.cssPanel.Controls.Add(this.cssClassNameLbl);
             this.cssPanel.Controls.Add(this.cssPositionTxt);
@@ -118,17 +118,17 @@
             this.cssPanel.Size = new System.Drawing.Size(148, 247);
             this.cssPanel.TabIndex = 6;
             this.cssPanel.Visible = false;
-            //
+            // 
             // cssClassNameTxt
-            //
+            // 
             this.cssClassNameTxt.Location = new System.Drawing.Point(4, 23);
             this.cssClassNameTxt.Name = "cssClassNameTxt";
             this.cssClassNameTxt.Size = new System.Drawing.Size(134, 20);
             this.cssClassNameTxt.TabIndex = 9;
             this.cssClassNameTxt.TextChanged += new System.EventHandler(this.cssClassNameTxt_TextChanged);
-            //
+            // 
             // cssClassNameLbl
-            //
+            // 
             this.cssClassNameLbl.ForeColor = System.Drawing.SystemColors.Highlight;
             this.cssClassNameLbl.Location = new System.Drawing.Point(4, 2);
             this.cssClassNameLbl.Name = "cssClassNameLbl";
@@ -136,17 +136,17 @@
             this.cssClassNameLbl.TabIndex = 8;
             this.cssClassNameLbl.TabStop = false;
             this.cssClassNameLbl.Text = "Class name";
-            //
+            // 
             // cssPositionTxt
-            //
+            // 
             this.cssPositionTxt.Location = new System.Drawing.Point(4, 209);
             this.cssPositionTxt.Name = "cssPositionTxt";
             this.cssPositionTxt.Size = new System.Drawing.Size(131, 20);
             this.cssPositionTxt.TabIndex = 7;
             this.cssPositionTxt.TextChanged += new System.EventHandler(this.cssPositionTxt_TextChanged);
-            //
+            // 
             // cssPositionLbl
-            //
+            // 
             this.cssPositionLbl.ForeColor = System.Drawing.SystemColors.Highlight;
             this.cssPositionLbl.Location = new System.Drawing.Point(1, 189);
             this.cssPositionLbl.Name = "cssPositionLbl";
@@ -154,17 +154,17 @@
             this.cssPositionLbl.TabIndex = 6;
             this.cssPositionLbl.TabStop = false;
             this.cssPositionLbl.Text = "Position";
-            //
+            // 
             // cssAttachTxt
-            //
+            // 
             this.cssAttachTxt.Location = new System.Drawing.Point(4, 162);
             this.cssAttachTxt.Name = "cssAttachTxt";
             this.cssAttachTxt.Size = new System.Drawing.Size(137, 20);
             this.cssAttachTxt.TabIndex = 5;
             this.cssAttachTxt.TextChanged += new System.EventHandler(this.cssAttachTxt_TextChanged);
-            //
+            // 
             // cssAttachbl
-            //
+            // 
             this.cssAttachbl.ForeColor = System.Drawing.SystemColors.Highlight;
             this.cssAttachbl.Location = new System.Drawing.Point(0, 142);
             this.cssAttachbl.Name = "cssAttachbl";
@@ -172,9 +172,9 @@
             this.cssAttachbl.TabIndex = 4;
             this.cssAttachbl.TabStop = false;
             this.cssAttachbl.Text = "Attachment";
-            //
+            // 
             // cssRepeatCbo
-            //
+            // 
             this.cssRepeatCbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cssRepeatCbo.FormattingEnabled = true;
             this.cssRepeatCbo.Items.AddRange(new object[] {
@@ -187,9 +187,9 @@
             this.cssRepeatCbo.Size = new System.Drawing.Size(134, 21);
             this.cssRepeatCbo.TabIndex = 3;
             this.cssRepeatCbo.SelectedIndexChanged += new System.EventHandler(this.cssRepeatCbo_SelectedIndexChanged);
-            //
+            // 
             // cssRepeatLbl
-            //
+            // 
             this.cssRepeatLbl.ForeColor = System.Drawing.SystemColors.Highlight;
             this.cssRepeatLbl.Location = new System.Drawing.Point(0, 95);
             this.cssRepeatLbl.Name = "cssRepeatLbl";
@@ -197,9 +197,9 @@
             this.cssRepeatLbl.TabIndex = 2;
             this.cssRepeatLbl.TabStop = false;
             this.cssRepeatLbl.Text = "Repeat";
-            //
+            // 
             // cssBackColorLbl
-            //
+            // 
             this.cssBackColorLbl.ForeColor = System.Drawing.SystemColors.Highlight;
             this.cssBackColorLbl.Location = new System.Drawing.Point(4, 49);
             this.cssBackColorLbl.Name = "cssBackColorLbl";
@@ -207,9 +207,9 @@
             this.cssBackColorLbl.TabIndex = 1;
             this.cssBackColorLbl.TabStop = false;
             this.cssBackColorLbl.Text = "Background color";
-            //
+            // 
             // cssBackColorTxt
-            //
+            // 
             this.cssBackColorTxt.Location = new System.Drawing.Point(4, 69);
             this.cssBackColorTxt.MaxLength = 6;
             this.cssBackColorTxt.Name = "cssBackColorTxt";
@@ -217,26 +217,26 @@
             this.cssBackColorTxt.TabIndex = 0;
             this.cssBackColorTxt.TextChanged += new System.EventHandler(this.cssBackColorTxt_TextChanged);
             this.cssBackColorTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cssBackColorTxt_KeyDown);
-            //
+            // 
             // htmlPanel
-            //
+            // 
             this.htmlPanel.Controls.Add(this.htmlAltText);
             this.htmlPanel.Controls.Add(this.htmlAltTextLbl);
             this.htmlPanel.Location = new System.Drawing.Point(0, 147);
             this.htmlPanel.Name = "htmlPanel";
             this.htmlPanel.Size = new System.Drawing.Size(148, 217);
             this.htmlPanel.TabIndex = 8;
-            //
+            // 
             // htmlAltText
-            //
+            // 
             this.htmlAltText.Location = new System.Drawing.Point(4, 24);
             this.htmlAltText.Name = "htmlAltText";
             this.htmlAltText.Size = new System.Drawing.Size(134, 20);
             this.htmlAltText.TabIndex = 1;
             this.htmlAltText.TextChanged += new System.EventHandler(this.htmlAltText_TextChanged);
-            //
+            // 
             // htmlAltTextLbl
-            //
+            // 
             this.htmlAltTextLbl.ForeColor = System.Drawing.SystemColors.Highlight;
             this.htmlAltTextLbl.Location = new System.Drawing.Point(3, 4);
             this.htmlAltTextLbl.Name = "htmlAltTextLbl";
@@ -244,9 +244,9 @@
             this.htmlAltTextLbl.TabIndex = 0;
             this.htmlAltTextLbl.TabStop = false;
             this.htmlAltTextLbl.Text = "Alt Text";
-            //
+            // 
             // encodeTypeLbl
-            //
+            // 
             this.encodeTypeLbl.ForeColor = System.Drawing.SystemColors.Highlight;
             this.encodeTypeLbl.Location = new System.Drawing.Point(3, 99);
             this.encodeTypeLbl.Name = "encodeTypeLbl";
@@ -254,9 +254,9 @@
             this.encodeTypeLbl.TabIndex = 4;
             this.encodeTypeLbl.TabStop = false;
             this.encodeTypeLbl.Text = "URI data type";
-            //
+            // 
             // fileFormatLbl
-            //
+            // 
             this.fileFormatLbl.ForeColor = System.Drawing.SystemColors.Highlight;
             this.fileFormatLbl.Location = new System.Drawing.Point(4, 4);
             this.fileFormatLbl.Name = "fileFormatLbl";
@@ -264,11 +264,11 @@
             this.fileFormatLbl.TabIndex = 0;
             this.fileFormatLbl.TabStop = false;
             this.fileFormatLbl.Text = "File format";
-            //
+            // 
             // Base64SaveConfigWidget
-            //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Controls.Add(this.cssPanel);
             this.Controls.Add(this.uriDataTypeCbo);
             this.Controls.Add(this.encodeTypeLbl);
